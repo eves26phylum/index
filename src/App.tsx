8292dcd83574abdc35c5dd48b1bcbffdad2bb2da
eves <275312891+eves26phylum@users.noreply.github.com>
2026-06-05 18:26:15 +1000

    Added app homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { useState } from 'react';
-import { BrowserRouter } from 'react-router';
-export function App() {
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router';
+import './App.css';
 
+export function App() {
   return <>
+    <header>
+      <div className="header">
+        <p>YOURNAMEGOESHERE</p>
+      </div>
+    </header>
+    <nav>
+
+    </nav>
+    <BrowserRouter>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
   </>;
 }
