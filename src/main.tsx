6a638db491397dc8a8ac1fd35543cfa6bc233758
eves <275312891+eves26phylum@users.noreply.github.com>
2026-06-05 18:15:16 +1000

    Using browser router via redirects

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import { App } from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function render() {
+  const root = document.getElementById('root');
+  if (!root) {
+    document.body.innerHTML = `
+      <h1>website malfunction</h1>
+      <p>wtfwtfwtfwtf</p>
+    `;
+    return window.alert("website malfunction wtfwtfwtf");
+  }
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+render();
