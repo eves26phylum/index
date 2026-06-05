85bdd65ff40ef517b4965487bb56f6fcac77fd85
eves <275312891+eves26phylum@users.noreply.github.com>
2026-06-05 18:56:41 +1000

    made it centered

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router";
+
 export function NotFound() {
-    return <div className="mainContent">
+    return <div className="mainContent notFound">
         <h1>Not Found</h1>
+        <p>Perhaps, try looking somewhere else.</p>
+        <Link to="/">Go Home</Link>
     </div>;
 }
