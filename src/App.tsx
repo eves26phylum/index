37b3d95000ced6ce76405bf0368c413f1a28430f
eves <275312891+eves26phylum@users.noreply.github.com>
2026-06-06 18:21:55 +1000

    Years left progressbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { BrowserRouter } from 'react-router';
 import './App.css';
 import { MenuBar } from './components/Header';
 import { AllRoutes } from './Routes';
-
+import { useState } from 'react';
+export function StartScreen({setHasLoaded}: {setHasLoaded: React.Dispatch<React.SetStateAction<boolean>>}) {
+  setTimeout(()=> {setHasLoaded(true)}, 1000);
+  return <></>;
+}
 export function App() {
+  const [hasLoaded, setHasLoaded] = useState<boolean>(false);
   return <>
+    { hasLoaded ?
     <BrowserRouter>
       <MenuBar/>
       <main>
@@ -13,5 +19,6 @@ export function App() {
       {/* <footer>
       </footer> */}
     </BrowserRouter>
+    : <StartScreen setHasLoaded={setHasLoaded}/>}
   </>;
 }
--- src/pages/DestructiveActions.tsx
+++ src/pages/DestructiveActions.tsx
@@ -1,17 +1,67 @@
 import { NavLink } from "react-router";
 import diskSpaceLeft from "../assets/images/diskSpaceLeft.png";
+import { useEffect, useRef, useState } from "react";
+
+function getUnixTimestampOfTime(targetYear: number, targetMonth: number, targetDay: number): number {
+    const formattedMonth = String(targetMonth).padStart(2, '0'); // left-pad hahaha
+    const formattedDay = String(targetDay).padStart(2, '0');
+    const isoString = `${targetYear}-${formattedMonth}-${formattedDay}T00:00:00+10:00`;
+    return Math.floor(Date.parse(isoString) / 1000);
+}
+function secondsToYears(seconds: number) {
+    return seconds / 31536000;
+}
+function getUltraDeadline() {
+    return getUnixTimestampOfTime(2029, 12, 30);
+}
+function getStartTask() {
+    return getUnixTimestampOfTime(2011, 12, 30);
+}
+function getEnd() {
+    return getUltraDeadline() - getStartTask();
+}
+function calculateUntilUltraDeadline(currentTime: number) {
+    return (currentTime - getStartTask()) / getEnd();
+}
+
+export function LearningProgressBar({progress, children}: {progress: number, children?: React.ReactNode}) {
+    return <div className="progressBar">
+        <div className="progressIns" style={{
+            width: `${progress * 100}%`
+        }}>
+            {children}
+        </div>
+    </div>
+}
 export function DestructiveActions() {
+    const calculateSimulatedTime = () => {
+        // return getUltraDeadline();
+        return Math.floor(Date.now() / 1000);
+    }
+    const [simulatedTime, setSimulatedTime] = useState<number>(calculateSimulatedTime());
+    const progress = Math.max(calculateUntilUltraDeadline(simulatedTime), 0);
+    useEffect(() => {
+        const id = setTimeout(() => {setSimulatedTime(calculateSimulatedTime())}, 1000);
+        return () => {clearTimeout(id)};
+    });
     return <div className="mainContent presentation">
+        <img src={diskSpaceLeft} className="behaveImage" alt="Disk Space Left on Macintosh—not much is left"/>
         <div className="default blog">
-            <h1>OS Information</h1>
             <p>Version 1 [up to date since 06/06/2026]</p>
-            <img src={diskSpaceLeft} className="behaveImage" alt="Disk Space Left on Macintosh—not much is left"/>
-            <p>my actual disk space storage</p>
-            <h2>Report Bugs in Eves26Phylum OS</h2>
-            <NavLink to="/email">Contact eves26phylum sole trader</NavLink>
-            <h2>Destructive Actions</h2>
-            <button>Destroy OS</button>
-            <button>Goodbye</button>
+            <NavLink to="/email">Report bugs or contact eves26phylum</NavLink>
+            <div className="group">
+                <strong>Production Time</strong>
+                <LearningProgressBar progress={progress}>
+                    <p className="yearsLeft">{`${secondsToYears(getEnd() - progress * getEnd()).toFixed(1)} years left`}</p>
+                </LearningProgressBar>
+            </div>
+            <div className="group">
+                <strong>Destructive Actions</strong>
+                <div className="group gap">
+                    <button className="os-option">Destroy OS</button>
+                    <button>Goodbye</button>
+                </div>
+            </div>
         </div>
     </div>;
 }
