238a0376ccc30d1b3d2670843c4db9332d799b73
eves <275312891+eves26phylum@users.noreply.github.com>
2026-06-06 15:09:05 +1000

    made the buttons smaller, not 100% safari filled and added contact

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,25 +2,38 @@ import { NavLink, type NavLinkProps } from "react-router";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye as faFilledEye, faClipboard as faFilledClipboard, type IconDefinition, faFloppyDisk as faFilledFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 import { faEye, faClipboard, faFloppyDisk } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 export interface HeaderLinkProps extends NavLinkProps {
     
 }
-export function IconAndTextCombo({filledIcon, nonFilledIcon, children}: {filledIcon: IconDefinition, nonFilledIcon: IconDefinition, children?: React.ReactNode}) {
-    return ({isActive}: {isActive: boolean}) => {return <><FontAwesomeIcon icon={ isActive ? filledIcon : nonFilledIcon }/><p>{children}</p></>};
+export interface IconTextComboDefinition {
+    filledIcon: IconDefinition,
+    nonFilledIcon?: IconDefinition,
+    children?: React.ReactNode
+}
+export function IconAndTextCombo({filledIcon, nonFilledIcon, children}: IconTextComboDefinition) {
+    const nonFilled = nonFilledIcon || filledIcon;
+    return ({isActive}: {isActive: boolean}) => {return <><FontAwesomeIcon icon={ isActive ? filledIcon : nonFilled }/><p>{children}</p></>};
 }
 export function HeaderLink({children, ...props}: HeaderLinkProps) {
     return <NavLink className={({isActive}) => {return isActive ? "nav-active nav-link" : "nav-link"}} {...props}>{children}</NavLink>;
 }
 export function MenuBar() {
-    return <><header>
-        <div className="header">
-            <nav>
-                <HeaderLink to="/">{IconAndTextCombo({filledIcon: faFilledClipboard, nonFilledIcon: faClipboard, children: "About Me"})}</HeaderLink>
-                <HeaderLink to="/blog">{IconAndTextCombo({filledIcon: faFilledEye, nonFilledIcon: faEye, children: "Projects"})}</HeaderLink>
-                <HeaderLink to="/dog_feeding_simulator">:3 DogFeedingSimulator</HeaderLink>
-                <HeaderLink to="/contact">{IconAndTextCombo({filledIcon: faFilledFloppyDisk, nonFilledIcon: faFloppyDisk, children: "Contact"})}</HeaderLink>
-            </nav>
-        </div>
-    </header>
+    return <>
+        <header>
+            <div className="header">
+                <nav>
+                    <HeaderLink to="/">{IconAndTextCombo({filledIcon: faFilledClipboard, nonFilledIcon: faClipboard, children: "About Me"})}</HeaderLink>
+                    <HeaderLink to="/blog">{IconAndTextCombo({filledIcon: faFilledEye, nonFilledIcon: faEye, children: "Projects"})}</HeaderLink>
+                    {/* <HeaderLink to="/dog_feeding_simulator">{IconAndTextCombo({filledIcon: faFilledEye, nonFilledIcon: faEye, children: "Projects"})}</HeaderLink> */}
+                    <HeaderLink to="/contact">{IconAndTextCombo({filledIcon: faFilledFloppyDisk, nonFilledIcon: faFloppyDisk, children: "Contact"})}</HeaderLink>
+                </nav>
+                <nav>
+                    <HeaderLink to="https://github.com/eves26phylum">{IconAndTextCombo({filledIcon: faGithub, children: "GitHub"})}</HeaderLink>
+                    <HeaderLink to="mailto:index@example.com">{IconAndTextCombo({filledIcon: faEnvelope, children: "Mail"})}</HeaderLink>
+                </nav>
+            </div>
+        </header>
     </>
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 export function HomePage() {
     return <div className="mainContent presentation">
         <div className="default blog">
-        <h1>eves26phylum Incorporated</h1>
-        <p>Explore the underdwindling possibilites of eveProduct™</p>
+            <h1>eves26phylum Incorporated</h1>
+            <p>Explore the underdwindling possibilites of eveProduct™</p>
         </div>
     </div>
 }
