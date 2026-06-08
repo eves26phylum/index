a55c149adcfa7950de0b2d93fbaf997de105e552
eves <275312891+eves26phylum@users.noreply.github.com>
2026-06-08 18:33:03 +1000

    enc2

--- src/pages/Mail.tsx
+++ src/pages/Mail.tsx
@@ -1,20 +1,16 @@
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router";
-
+import { mail, mailAddress } from "../assets/documents/mail";
 type clipboardCopy = {
     copy: string
 }
 export function MailMe({ClipboardCopyButton}: {ClipboardCopyButton: React.ComponentType<clipboardCopy>}) {
-    const mailAddress = [":", "o", "t", "l", "i", "a", "m"].reverse().join("");
-    function decrypt_mail() {
-        let s=3203383023;const b:number[]=[];s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);s=((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))^((((s^(s<<13)>>>0)^((s^(s<<13)>>>0)>>>17))<<5)>>>0);b.push(s&0xFF);return[220,86,63,151,245,244,169,181,44,32,243,128,35,153,185,127,163].map((v,i)=>String.fromCharCode(v^b[i])).join("");
-    }
     return <div className="mainContent presentation">
         <div className="default blog centered">
             <h1>My Contacts</h1>
             <div>
-                <p>Email me at <code>{decrypt_mail()}<ClipboardCopyButton copy={decrypt_mail()}/></code></p>
-                <NavLink to={`${mailAddress}${decrypt_mail()}`}>Send Mail</NavLink>
+                <p>Email me at <code>{mail}<ClipboardCopyButton copy={mail}/></code></p>
+                <NavLink to={`${mailAddress}${mail}`}>Send Mail</NavLink>
             </div>
         </div>
     </div>
