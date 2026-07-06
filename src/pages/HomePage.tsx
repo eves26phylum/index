c3c4a06b5f0c2757972406e58b1ae8b366ea734b
eves <275312891+eves26phylum@users.noreply.github.com>
2026-07-06 23:46:57 +1000

    enhanced experience to brevity

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,16 @@
 import { NavLink } from "react-router";
 import { calculateUntilUltraDeadline, getEnd, secondsToYears } from "../utilities/age";
 import Tooltip from "../components/Tooltip";
+import myselfondiscord from "../assets/images/myselfondiscord.png";
 
 export function HomePage() {
     return <div className="mainContent presentation">
         <div className="blog end">
+            <img src={myselfondiscord} className="behaveImage"/>
             <div className="default middle row">
                 <h1>eves26phylum</h1>
                 <div className="default row">
-                    <NavLink target="_blank" to="/github">GitHub</NavLink>
+                    <NavLink target="_blank" to="https://github.com/eves26phylum">GitHub</NavLink>
                     <NavLink to="/destructive_actions">Settings</NavLink>
                     <NavLink target="_blank" to="https://discord.gg/5x7aUsNp2V">Discord</NavLink>
                 </div>
@@ -19,9 +21,7 @@ export function HomePage() {
                 <NavLink to="/what_i_do">Looking for my software developer information?</NavLink>
             </div>
             <div className="default">
-                <strong>⚠️ VERY IMPORTANT ANNOUNCEMENT ⚠️</strong>
-                <p>(satire)</p>
-                {Array(500).fill(<NavLink to="/futon_gpt">FutonGPT Public Release Announcement: A New Area of Efficiency</NavLink>)}
+                <NavLink to="/futon_gpt">FutonGPT Public Release Announcement: A New Area of Efficiency</NavLink>
             </div>
         </div>
     </div>
