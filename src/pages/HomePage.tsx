4a6288351525172ec8a0612d439830a2b846dc4c
eves <275312891+eves26phylum@users.noreply.github.com>
2026-07-07 00:05:49 +1000

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,12 +16,12 @@ export function HomePage() {
                 </div>
             </div>
             <div className="default">
-                <p>I am a {Math.floor(secondsToYears(Math.max(calculateUntilUltraDeadline(Math.floor(Date.now() / 1000)), 0) * getEnd()))} years old male who enjoys using MacBooks
+                <p>I am a {Math.floor(secondsToYears(Math.max(calculateUntilUltraDeadline(Math.floor(Date.now() / 1000)), 0) * getEnd()))} years old male who enjoys using MacBooks and had an addiction to ROBLOX/Social Media for some of my teenage years
                 </p>
                 <NavLink to="/what_i_do">Looking for my software developer information?</NavLink>
             </div>
             <div className="default">
-                <NavLink to="/futon_gpt">FutonGPT Public Release Announcement: A New Area of Efficiency</NavLink>
+                <NavLink to="/futon_gpt">FutonGPT Public Release Announcement: A New Era of Efficiency</NavLink>
             </div>
         </div>
     </div>
