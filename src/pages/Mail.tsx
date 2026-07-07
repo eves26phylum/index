3650c91eddcbbfcc58db8b8f57982ce6dd724179
eves <275312891+eves26phylum@users.noreply.github.com>
2026-07-07 21:33:05 +1000

    email improvements

--- src/pages/Mail.tsx
+++ src/pages/Mail.tsx
@@ -5,12 +5,14 @@ type clipboardCopy = {
     copy: string
 }
 export function MailMe({ClipboardCopyButton}: {ClipboardCopyButton: React.ComponentType<clipboardCopy>}) {
+    const split_1 = mail.split("@");
+    const split_2 = split_1[1].split(".");
     return <div className="mainContent presentation">
         <div className="default end blog centered">
             <h1>My Contacts</h1>
             <div>
-                <p>Email me at <code>{mail}<ClipboardCopyButton copy={mail}/></code></p>
-                <NavLink to={`${mailAddress}${mail}`}>Send Mail</NavLink>
+                <p>Email me at <code>{`${split_1[0]} `}AT_SYMBOL<span>{`${split_2[0]}`}</span>DOT_PUNCTUATION<span>{`${split_2[1]}`}</span><ClipboardCopyButton copy={mail}/></code></p>
+                {/* <NavLink to={`${mailAddress}${mail}`}>Send Mail</NavLink> */}
             </div>
         </div>
     </div>
