4b5886b2f77c435a9d28ed31d1934d98d2f7018d
eves <275312891+eves26phylum@users.noreply.github.com>
2026-07-07 22:39:55 +1000

    eah

--- src/pages/Mail.tsx
+++ src/pages/Mail.tsx
@@ -1,8 +1,9 @@
 import { faAt, faCircle, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { getMailAddress } from "../assets/documents/mail";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import { calculateUntilUltraDeadline, getEnd, secondsToYears } from "../utilities/age";
 type clipboardCopy = {
     copy: string
 }
@@ -11,6 +12,7 @@ export function MailMe({ClipboardCopyButton}: {ClipboardCopyButton: React.Compon
     // let split_1 = Array(170).fill("What the fuck are you doing");
     // let split_2 = Array(170).fill("What the fuck are you doing");
     const [boi, setSplit] = useState<[string[],string[]]>([["no", "no"], ["no", "no"]]);
+    const navigate = useNavigate();
     const [split_1, split_2] = boi;
     const [mail, setMail] = useState<string>("hah");
     const handleReveal = () => {
@@ -20,6 +22,7 @@ export function MailMe({ClipboardCopyButton}: {ClipboardCopyButton: React.Compon
         setShown(true);
     };
     const [part, setPart] = useState(0);
+    const [firstQuestion, setFirstQuestion] = useState<string>("");
     useEffect(() => {
         if (part !== 1) return;
         const id = setTimeout(()=> {
@@ -59,24 +62,33 @@ export function MailMe({ClipboardCopyButton}: {ClipboardCopyButton: React.Compon
                 { part === 0 ?
                 <button className="showMail" onClick={() => {
                     setPart(part => part + 1);
-                }}>Click to show my email address</button> 
+                }}>Double-click to show my email address</button> 
                 : 
                 <>
                 { part === 1 ?
                 <button className="showMail red" onClick={() => {
                     setPart(part => part + 1);
-                }}>Click AGAIN show my email address...</button> 
+                }}>Double-click to show my email address...</button> 
                 : <>
                 <p>Want to get my email? Answer this question to prove you're not one of them web scrapers.</p>
-                <div>
-                    <p>Question One</p>
+                <p className="red">If you answer wrong, you will get redirected to the homepage.</p>
+                <p>I am a {Math.floor(secondsToYears(Math.max(calculateUntilUltraDeadline(Math.floor(Date.now() / 1000)), 0) * getEnd()))} years old male</p>
+                <div className="theform">
+                    
+                    <p>How old am I?</p>
                     <input type="text" placeholder="Answer." onChange={(event) => {
                         const element = event.target;
+                        setFirstQuestion(element.value);
                     }}/>
                 </div>
-                <button className="showMail" onClick={() => {
+                <button className="showMail red" onClick={() => {
+                    const age= Math.floor(secondsToYears(Math.max(calculateUntilUltraDeadline(Math.floor(Date.now() / 1000)), 0) * getEnd()));
+                    if (firstQuestion !== age.toString()) {
+                        navigate("/");
+                        return;
+                    }
                     handleReveal();
-                }}>Grant Access to Email Address</button> 
+                }}>🔒 Test if I'm correct. I want to be granted access.</button> 
                 </>
                 }
                 </>
