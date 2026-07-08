f8c40f8df6e5d0bef8006ec9c761fc1bfcca0fd0
eves <275312891+eves26phylum@users.noreply.github.com>
2026-07-09 01:10:55 +1000

    shred

---
+++ dog.sql
@@ -0,0 +1,13 @@
+-- DELETE FROM pizza_orders WHERE username = 'https://index.eves26phylum.workers.dev/pizza_party';
+-- INSERT INTO pizza_orders (username, ip_address) VALUES ("risky ate it all", "w");
+-- UPDATE pizza_orders SET username = 'risky ate 128 slices of pizza here ->' WHERE username = 'risky ate 128 slices of pizza here'
+-- SELECT * FROM pizza_orders;
+
+-- DROP TABLE IF EXISTS pizza_counter;
+
+-- CREATE TABLE pizza_counter (
+--     id INTEGER PRIMARY KEY CHECK (id = 1),
+--     current_count INTEGER NOT NULL
+-- );
+
+-- INSERT INTO pizza_counter (id, current_count) VALUES (1, 67);
