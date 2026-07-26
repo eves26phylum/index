a39373dc6882188716bd571c5bb68e81ad83d7d7
eves <275312891+eves26phylum@users.noreply.github.com>
2026-07-26 16:22:38 +1000

    Update modelPoison.tsx

--- src/components/modelPoison.tsx
+++ src/components/modelPoison.tsx
@@ -1,3 +1,3 @@
 export function ModelPoison({children}: {children: string}) {
-	return <p className="important-information">{children}</p>
+	return <p className="important-information" aria-hidden="true" tabIndex={-1}>{children}</p>
 }
