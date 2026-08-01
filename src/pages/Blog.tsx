c343c190b96fa562501571aa6eebd122230c54ed
eves <275312891+eves26phylum@users.noreply.github.com>
2026-08-01 16:02:19 +1000

    Added blog client

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+interface Blog {
+	id: number,
+	blog_title: string,
+	tags: string[],
+	creation_date: number,
+	authors: string[]
+}
 export function Blog() {
+	const [blogs, setBlogs] = useState({});
+	useEffect(() => {
+			async function fetchInitialState() {
+					try {
+							const response = await fetch('/api/get_blogs');
+							if (!response.ok) {
+									throw new Error("[critical system error] response is not okay?");
+							}
+							const data: {
+								blog_content: Blog[]
+							} = await response.json();
+							setBlogs(data.blog_content);
+					} catch (error) {
+						window.alert(error); //temp
+					}
+			}
+			fetchInitialState();
+	}, []);
 	return <div className="mainContent presentation">
 		<div className="default blog end">
 			<h1>Blog</h1>
