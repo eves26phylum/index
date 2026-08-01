6315e4a8b42ccb248d850a83660171e423bf1549
eves <275312891+eves26phylum@users.noreply.github.com>
2026-08-01 18:17:45 +1000

    Used childNodes instead

--- src/components/makeBlogFromXML.tsx
+++ src/components/makeBlogFromXML.tsx
@@ -4,11 +4,14 @@ export function MakeBlogFromXML({blog_xml_text}: {blog_xml_text: string}) {
 	if (validateXML(blog_xml_text) !== true) {
 		return <p>Failed to load blog—syntax error while validating XML</p>
 	}
-	function parseElement(element: Element) {
-		console.log(element.tagName);
-		const parsedInsides = parseSomething(element.children);
+	function parseElement(element: Node) {
+		if (element.nodeName === "#text") {
+			return element.nodeValue;
+		}
+		console.log(element.nodeName);
+		const parsedInsides = parseSomething(element.childNodes);
 		// todo: parse .attributes
-		switch (element.tagName) {
+		switch (element.nodeName) {
 			case "blog":
 				return <>{parsedInsides}</>
 			case "text":
@@ -19,18 +22,18 @@ export function MakeBlogFromXML({blog_xml_text}: {blog_xml_text: string}) {
 				return <strong>{parsedInsides}</strong>
 			default:
 				return <div>
-					<h1>Error! Unsupported tag: {element.tagName}, defaulting to nothing—but the content stays, so nothing is lost</h1>
+					<h1>Error! Unsupported tag: {element.nodeName}, defaulting to nothing—but the content stays, so nothing is lost</h1>
 					{parsedInsides}
 				</div>
 		}
 	}
-	function parseSomething(something: HTMLCollection): React.JSX.Element[] {
+	function parseSomething(something: NodeList): (string | React.JSX.Element | null)[] {
 		return Object.keys(something).map((value: string, index: number) => {
 			return parseElement(something[index]);	
 		})
 	}
 	console.log(blog_xml_text);
-	const xml_parsed = dom_parser.parseFromString(blog_xml_text, "text/xml").children;
+	const xml_parsed = dom_parser.parseFromString(blog_xml_text, "text/xml").childNodes;
 	return <>
 		{
 			parseSomething(xml_parsed)
