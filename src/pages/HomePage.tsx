e3b7318aa5c05aee63c9c5418a2e0b32a9fa6eb2
eves26phylum <eves26phylum@noreply.codeberg.org>
2026-08-27 22:24:18 +1000

    big rabbit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -51,6 +51,9 @@ export function HomePage() {
 									</div>
 								</div>
             </div>
+						<div className="rabbit_that_takes_up_everything_and_is_only_for_filler_and_serves_no_other_purpose">
+							🐇
+						</div>
         </div>
     </div>
 }
